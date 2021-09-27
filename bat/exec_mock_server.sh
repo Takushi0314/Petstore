cae7c21adc820dd22d91b09dc2ba25a07694d07a
Takushi Nishiyama <takushi.nishiyama.0314@gmail.com>
2021-09-27 12:14:27 +0900

    swagger mockserver追加

---
+++ bat/exec_mock_server.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+# シェルファイルが存在するファイルパスの、ひとつ上(Git Root)移動する
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+# COMPOSE_FILEの指定
+export COMPOSE_FILE=docker-compose.dev.yml
+
+# docker-composeの実行
+docker-compose up mock_server -d
